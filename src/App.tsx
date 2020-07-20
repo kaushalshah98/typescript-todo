837e1feab3afa2abee8ae98a926e21f5032e078c
Kaushal Shah <kaushalshah589@gmail.com>
2020-07-20 13:54:04 +0530

    custom hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,29 @@
-import React, { Component, Fragment, ChangeEvent } from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import NavBar from "./components/navbar/navbar";
 import Container from "./components/to-do-container/container";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { IAppState, IAppProps, INewItem } from "./shared/interface";
+import useToDoState from "./stateMaintainer/state";
+
 library.add(faTrash);
-class App extends Component<IAppProps, IAppState>{
-  constructor(props: IAppProps) {
-    super(props);
-    this.state = {
-      list: [],
-      newItem: {
-        key: 0,
-        value: "",
-      },
-    };
-  }
-  onDeleteItem = (key: number) => {
-    const list = this.state.list.filter((item: INewItem) => item.key !== key);
-    this.setState({ list });
-  };
-  onAddItem = (e: ChangeEvent) => {
-    console.log(typeof (e));
-    e.preventDefault();
-    const { newItem } = this.state;
-    if (newItem.value !== "") {
-      const list = [...this.state.list, newItem];
-      this.setState({ list, newItem: { key: 0, value: "" } });
-    }
-  };
-  onUpdateItem = (key: number, value: string) => {
-    const { list } = this.state;
-    list.forEach((item) => {
-      if (item.key === key) {
-        item.value = value;
-      }
-    });
-    this.setState({ list });
-  };
-  handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value }: { value: string } = e.target;
-    const key = Date.now();
-    this.setState({ newItem: { value, key } });
-  };
-  render() {
-    return (
-      <Fragment>
-        <NavBar totalItems={this.state.list.length}></NavBar>
-        <Container
-          list={this.state.list}
-          newItem={this.state.newItem}
-          onUpdateItem={this.onUpdateItem}
-          onHandleInput={this.handleInput}
-          onDeleteItem={this.onDeleteItem}
-          onAddItem={this.onAddItem}
-        ></Container>
-      </Fragment>
-    );
-  }
+
+const App = () => {
+  const { list, newItem, handleInput, onUpdateItem, onAddItem, onDeleteItem } = useToDoState();
+  return (
+    <Fragment>
+      <NavBar totalItems={list.length}></NavBar>
+      <Container
+        list={list}
+        newItem={newItem}
+        onUpdateItem={onUpdateItem}
+        onHandleInput={handleInput}
+        onDeleteItem={onDeleteItem}
+        onAddItem={onAddItem}
+      ></Container>
+    </Fragment>
+  );
+
 }
 
 export default App;
---
+++ src/stateMaintainer/state.tsx
@@ -0,0 +1,40 @@
+import React, { ChangeEvent, useState } from 'react'
+import { INewItem } from '../shared/interface';
+
+const useToDoState = () => {
+    const [list, setList]: [INewItem[], any] = useState([]);
+    const [newItem, setNewItem]: [INewItem, any] = useState({ key: 0, value: '' })
+
+    const onDeleteItem = (key: number) => {
+        const newList: any = list.filter((item: INewItem) => item.key !== key);
+        setList(newList);
+    };
+
+    const onAddItem = (e: ChangeEvent) => {
+        e.preventDefault();
+        if (newItem.value !== "") {
+            const newList: any = [...list, newItem];
+            setList(newList);
+            setNewItem({ key: 0, value: "" })
+        }
+    };
+    const onUpdateItem = (key: number, value: string) => {
+        const newList = [...list];
+        newList.forEach((item: INewItem) => {
+            if (item.key === key) {
+                item.value = value;
+            }
+        });
+        setList(newList);
+    };
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value }: { value: string } = e.target;
+        const key = Date.now();
+        setNewItem({ value, key })
+    };
+
+    return { list, newItem, handleInput, onUpdateItem, onAddItem, onDeleteItem }
+}
+
+export default useToDoState;
